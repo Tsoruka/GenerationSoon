f6e156365cc4b6841976160bca033dd2ed03ed4f
Massimiliano <massicaccetta@gmail.com>
2021-12-23 15:47:02 +0100

    get

--- src/main/java/it/generationsoon/control/DettaglioFilmServlet.java
+++ src/main/java/it/generationsoon/control/DettaglioFilmServlet.java
@@ -39,7 +39,7 @@ public class DettaglioFilmServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		int filmId = 3;
 		try {
 			//creazione di un oggetto tipo film a partire dal filmService
