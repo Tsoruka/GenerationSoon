ab9bbff2386287fc9f609404e4b3b82f8c751efa
ceciliagradnik <ceciliagradnik@mbp-di-cecilia>
2021-12-30 10:17:59 +0000

    servlet di logout da richiamare con bottoni poi

---
+++ src/main/java/it/generationsoon/control/LogoutServlet.java
@@ -0,0 +1,33 @@
+package it.generationsoon.control;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LogoutServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			request.getSession().invalidate();
+			response.sendRedirect("login.jsp");
+		
+	}
+
+}
